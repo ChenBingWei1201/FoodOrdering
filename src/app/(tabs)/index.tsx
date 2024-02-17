ba20ffb5a795ca8ad0f05c421d71e00cec1d778d
ChenBingWei1201 <rain4030633@gmail.com>
2024-02-17 14:43:18 +0800

    4. Type Safe Components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import products from "@/assets/data/products";
-import ProductListItem from "../../components/ProductListItem";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   return (
