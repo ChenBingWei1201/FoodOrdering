d886e1e3772a87d402343e5f47089323f08bc1c0
ChenBingWei1201 <rain4030633@gmail.com>
2024-02-18 00:39:51 +0800

    9. Admin Site

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,71 @@
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import Product from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import type { PizzaSize } from "@/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailScreen = () => {
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  const product = Product.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) return;
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return (
+      <View>
+        <Text style={{ fontSize: 20 }}>Product Not Found</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    // marginTop: "auto",
+  },
+});
+
+export default ProductDetailScreen;
---
+++ src/app/(user)/menu/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
+    </View>
+  );
+}
